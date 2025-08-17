378b37d02df780ac234f3d18198c352da03bdf05
debvm <you@example.com>
2025-08-17 12:17:33 -0300

    add set url no ssh.sh

--- ssh.sh
+++ ssh.sh
@@ -38,5 +38,4 @@ else
 fi
 
 echo "✅ Chave SSH criada com sucesso!"
-echo "👉 Agora adicione a chave pública na sua conta Git (GitHub/GitLab, etc)."
-
+echo "use :git remote set-url origin git@github.com:user/rep.git"
